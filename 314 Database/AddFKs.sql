713f40357ef217d24d50ac6dcd2b0ed859b44ddf
Tim <timothy.j.yates1995@gmail.com>
2018-05-02 20:51:08 +1000

    Update AddFKs.sql

--- 314 Database/AddFKs.sql
+++ 314 Database/AddFKs.sql
@@ -1,23 +1,36 @@
 ALTER TABLE users
-ADD FOREIGN KEY (location_id) REFERENCES location(location_id);
+ADD FOREIGN KEY (location_id) REFERENCES location(location_id)
+ON DELETE CASCADE;
 
 ALTER TABLE admins
-ADD FOREIGN KEY (users_id) REFERENCES users(users_id);
+ADD FOREIGN KEY (users_id) REFERENCES users(users_id)
+ON DELETE CASCADE;
 
 ALTER TABLE car
-ADD FOREIGN KEY (users_id) REFERENCES users(users_id);
+ADD FOREIGN KEY (users_id) REFERENCES users(users_id)
+ON DELETE CASCADE;
+
+ALTER TABLE car_rating
+ADD FOREIGN KEY (car_id) REFERENCES car(car_id)
+ON DELETE CASCADE;
 
 ALTER TABLE location
-ADD FOREIGN KEY (users_id) REFERENCES users(users_id);
+ADD FOREIGN KEY (users_id) REFERENCES users(users_id)
+ON DELETE CASCADE;
 
 ALTER TABLE reservation
-ADD FOREIGN KEY (renter) REFERENCES users(users_id);
+ADD FOREIGN KEY (owner) REFERENCES users(users_id)
+ON DELETE CASCADE;
 ALTER TABLE reservation
-ADD FOREIGN KEY (rentee) REFERENCES users(users_id);
+ADD FOREIGN KEY (renter) REFERENCES users(users_id)
+ON DELETE CASCADE;
 ALTER TABLE reservation
-ADD FOREIGN KEY (car_id) REFERENCES car(car_id);
+ADD FOREIGN KEY (car_id) REFERENCES car(car_id)
+ON DELETE CASCADE;
 
 ALTER TABLE message
-ADD FOREIGN KEY (sentby) REFERENCES users(users_id);
+ADD FOREIGN KEY (sentby) REFERENCES users(users_id)
+ON DELETE CASCADE;
 ALTER TABLE message
-ADD FOREIGN KEY (sentto) REFERENCES users(users_id);
+ADD FOREIGN KEY (sentto) REFERENCES users(users_id)
+ON DELETE CASCADE;
